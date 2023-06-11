ef8cd8c05929edd5c48ea651cdd517eb1a2345b0
Vivi Tseng <vii0726@gmail.com>
2023-06-10 21:06:52 -0700

    feat: add google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
 import Providers from './providers'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
