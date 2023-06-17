c8ddad48235abd0765906eef30baa41f08f324d4
Vivi Tseng <vii0726@gmail.com>
2023-06-16 17:11:05 -0700

    feat: add swiper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,71 @@
 'use client'
 import Image from 'next/image'
 import styled from 'styled-components'
-import { Navbar } from '@/components/Navbar'
 import { Button } from '@/components/Button'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { EffectCards } from 'swiper'
+import 'swiper/css/effect-cards'
 
 export default function Home() {
   return (
     <Container>
-      <Navbar />
+      <TitleWrapper>
+        <Title>The Movie Planet</Title>
+        <Description>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium,
+          repudiandae!
+        </Description>
+        <Button>explore</Button>
+      </TitleWrapper>
 
-      <Title>The Movie Planet</Title>
-      <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium,
-        repudiandae!
-      </div>
-      <Button>explore</Button>
+      <SwiperWrapper>
+        <Swiper
+          style={{ width: '100%', height: '100%' }}
+          effect={'cards'}
+          grabCursor={true}
+          modules={[EffectCards]}
+          loop={true}
+          // prevent swiping multiple slides at once
+          onRealIndexChange={(swiper) => {
+            swiper.allowTouchMove = false
+            swiper.unsetGrabCursor()
+          }}
+          onTouchEnd={(swiper) => {
+            swiper.allowTouchMove = true
+          }}
+        >
+          {Array.from(Array(5)).map((_, i) => (
+            <SwiperSlide key={i} style={{ background: 'orange' }}>
+              slide {i}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </SwiperWrapper>
     </Container>
   )
 }
 
 const Container = styled.div`
   padding: 24px 36px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 36px;
 `
+
+const TitleWrapper = styled.div``
 
 const Title = styled.h1`
   font-family: 'Poppins', sans-serif;
   font-weight: 900;
   font-size: 60px;
+`
+const Description = styled.div`
+  line-height: 1.5;
+`
+
+const SwiperWrapper = styled.div`
+  width: 240px;
+  height: 320px;
+  margin-right: 50px;
 `
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,9 @@
 'use client'
 import { useEffect } from 'react'
 import { useUserStore } from '@/lib/store'
+import { register } from 'swiper/element/bundle'
+import 'swiper/swiper.min.css'
+register()
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   // const { updateLang } = useUserStore()
